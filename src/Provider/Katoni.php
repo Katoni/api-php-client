0bd2d7db5346d502d560422cda6bdcfc1829a04f
Martin Skovvang Petersen <martin@katoni.dk>
2016-08-29 15:18:58 +0200

    Initial commit.

---
+++ src/Provider/Katoni.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Katoni\OAuth2\Client\Provider;
+
+use Katoni\OAuth2\Client\Provider\Exception\KatoniIdentityProviderException;
+use League\OAuth2\Client\Provider\AbstractProvider;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use League\OAuth2\Client\Token\AccessToken;
+use League\OAuth2\Client\Tool\BearerAuthorizationTrait;
+use Psr\Http\Message\ResponseInterface;
+
+class Katoni extends AbstractProvider
+{
+    use BearerAuthorizationTrait;
+
+    /**
+     * Get authorization url to begin OAuth flow
+     *
+     * @return string
+     */
+    public function getBaseAuthorizationUrl()
+    {
+        return 'https://api.katoni.com/oauth2/authorize';
+    }
+
+    /**
+     * Get access token url to retrieve token
+     *
+     * @param  array $params
+     *
+     * @return string
+     */
+    public function getBaseAccessTokenUrl(array $params)
+    {
+        return 'https://api.katoni.com/oauth2/access_token';
+    }
+
+    /**
+     * Get provider url to fetch user details
+     *
+     * @param  AccessToken $token
+     *
+     * @return string
+     */
+    public function getResourceOwnerDetailsUrl(AccessToken $token)
+    {
+        return 'https://api.katoni.com/v1/account';
+    }
+
+    /**
+     * Get the default scopes used by this provider.
+     *
+     * This should not be a complete list of all scopes, but the minimum
+     * required for the provider user interface!
+     *
+     * @return array
+     */
+    protected function getDefaultScopes()
+    {
+        return [];
+    }
+
+    /**
+     * Check a provider response for errors.
+     *
+     * @link   https://developer.github.com/v3/#client-errors
+     * @link   https://developer.github.com/v3/oauth/#common-errors-for-the-access-token-request
+     * @throws IdentityProviderException
+     * @param  ResponseInterface $response
+     * @param  string $data Parsed response data
+     * @return void
+     */
+    protected function checkResponse(ResponseInterface $response, $data)
+    {
+        if ($response->getStatusCode() >= 400) {
+            throw KatoniIdentityProviderException::clientException($response, $data);
+        } elseif (isset($data['error'])) {
+            throw KatoniIdentityProviderException::oauthException($response, $data);
+        }
+    }
+
+    /**
+     * Generate a user object from a successful user details request.
+     *
+     * @param array $response
+     * @param AccessToken $token
+     * @return \League\OAuth2\Client\Provider\ResourceOwnerInterface
+     */
+    protected function createResourceOwner(array $response, AccessToken $token)
+    {
+        return new KatoniResourceOwner($response);
+    }
+}
